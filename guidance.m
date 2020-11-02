d13efef67857eba86156a7040974b222356b37ea
StianJakobsen <stian.mjelde20@hotmail.com>
2020-11-02 14:37:14 +0100

    psi_d blit kompleks

---
+++ guidance.m
@@ -0,0 +1,9 @@
+function chi_d = guidance(pos, x_t, y_t, x_ref, y_ref, R_los)
+    x = pos(1); y = pos(2);
+    [~, ~, y_e] = crosstrack(x_t, y_t, x_ref, y_ref, x, y);
+    lookahead = sqrt(R_los^2-y_e^2);
+    K_p = 1/lookahead;
+    pi_p = atan2(y_t-y_ref, x_t-x_ref);
+    chi_d = pi_p - atan(K_p*y_e);
+end
+
